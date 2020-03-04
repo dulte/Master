a73deaf69c206aabf889f19fa1832ede3cfc6df1
Daniel Heinesen <daniehei@stjerne16.uio.no>
2020-03-04 13:55:48 +0100

    Adds many of the functions needed for two body system

--- src/Python/ETInterpolater.py
+++ src/Python/ETInterpolater.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 import h5py
 import subprocess
 
+from warnings import simplefilter
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+
 
 from postcactus.simdir import SimDir
 from postcactus import visualize as viz
@@ -17,7 +21,7 @@ TODO:
 - Take care of symmetries from ET [~]
     - As of now this only takes abs, but it needs to get symmetry axis --if any.
 - Make sure boudaries are handled correctly! [-]
-    - PostCactus cannot extrapolate outside the ET grid. 
+    - PostCactus cannot extrapolate outside the ET grid.
       So this grid must be larger than highest coll point
 """
 
@@ -27,6 +31,8 @@ class ETInterpolater:
         self.sd = SimDir(dir)
         self.nb_bodies = nb_bodies
 
+        self.dir = dir
+
         self.xlim = [0,0]
         self.ylim = [0,0]
         self.zlim = [0,0]
@@ -35,15 +41,15 @@ class ETInterpolater:
     def read_ET_quantity(self,quantity, geometry, iteration, dimentions=3, order=4):
         if dimentions == 2:
             grid = self.sd.grid.xy.read(quantity, iteration, geom=geometry, order=order)
-            
+
         elif dimentions == 3:
             grid = self.sd.grid.xyz.read(quantity, iteration, geom=geometry, order=order)
         else:
             raise ValueError("Number of dimentions should be 2 or 3!")
-        
+
         print "[+] %s Successfully Read" %quantity
         return grid
-    
+
 
     def make_geometry(self, corner, n_pts):
         corner1 = [-i for i in corner]
@@ -55,17 +61,18 @@ class ETInterpolater:
         geo = gd.RegGeom([n_pts]*len(corner), corner, x1=corner1)
         print "[+] Geomentry Successfully Made"
         return geo
-    
+
 
 
     def make_test_plot(self, quantity):
         g = self.make_geometry([-50,-50], 400)
         q = self.read_ET_quantity(quantity, g, 0, dimentions=2)
 
-        n = 100
+        n = 300
+        end = 10
         q_inter = np.zeros((n,n))
-        x = np.linspace(-50,50, n)
-        y = np.linspace(-50,50, n)
+        x = np.linspace(-end, end, n)
+        y = np.linspace(-end, end, n)
 
         for i in range(n):
             for j in range(n):
@@ -77,13 +84,45 @@ class ETInterpolater:
         plt.show()
 
 
+    def make_test_bbh_plot(self, quantity, p1, p2, r1, r2):
+        g = self.make_geometry([-50,-50], 400)
+        q = self.read_ET_quantity(quantity, g, 0, dimentions=2)
+
+        n = 600
+        end = 10
+        q_inter = np.zeros((n,n))
+        x = np.linspace(-end, end, n)
+        y = np.linspace(-end, end, n)
+
+        th = np.linspace(0,2*np.pi,100)
 
 
-    def get_values_at_coll_points(self, interpolated_quantity, test=False):
+        for i in range(n):
+            for j in range(n):
+                q_inter[j,i] = q(np.array([self.desymmetrize_coord(x[i]), self.desymmetrize_coord(y[j])]))
+                q_inter[j,i] *= self.super_gaussian(x[i], y[j], 0, p1, r1)*self.super_gaussian(x[i], y[j], 0, p2, r2)
+
+
+        xx = p1[0] + r1*np.cos(th)
+        yy = p1[1] + r1*np.sin(th)
+        plt.plot(xx, yy, linestyle=':', color='black')
+
+        xx = p2[0] + r2*np.cos(th)
+        yy = p2[1] + r2*np.sin(th)
+        plt.plot(xx, yy, linestyle=':', color='black')
+
+        plt.plot(p1[0], p1[1], "r*")
+        plt.plot(p2[0], p2[1], "r*")
+
+        plt.pcolormesh(x,y,q_inter)
+        plt.colorbar()
+        plt.show()
+
+
+    def get_values_at_coll_points(self, interpolated_quantity,smooth=False, bh_pos=[0,0,0], bh_rad=0,bh_pos2=[0,0,0], bh_rad2=0, test=False):
         q = interpolated_quantity
-        xx, yy, zz = self.get_coll_points()
+        xx, yy, zz = self.get_coll_points(bh_pos)
 
-        print xx[0][1][3][4]
 
         r_test = []
         x_test = []
@@ -108,30 +147,38 @@ class ETInterpolater:
                         y = self.bound_coord(yy[index][k][j][r], "y")
                         z = self.bound_coord(zz[index][k][j][r], "z")
 
-                        if x != z:
-                            print x,y,z
-                        
+
+
                         inter_q = q([x,y,z])
+                        if smooth:
+                            inter_q *= self.super_gaussian(x,y,z, bh_pos, bh_rad)
+                            inter_q *= self.super_gaussian(x,y,z, bh_pos2, bh_rad2)
+
                         values[index][k][j][r] = inter_q
 
-                        r_test.append(np.sqrt(x**2 + y**2 + z**2))
-                        x_test.append(x)
-                        y_test.append(y)
-                        z_test.append(z)
-                        q_test.append(inter_q)
-                        #if inter_q == 0:
-                        #    print x,y,z
+                        if test:
+
+                            r_test.append(np.sqrt(x**2 + y**2 + z**2))
+                            x_test.append(x)
+                            y_test.append(y)
+                            z_test.append(z)
+                            q_test.append(inter_q)
+                            #if inter_q == 0:
+                            #    print x,y,z
+
                         flatten_values.append(inter_q)
 
         print "[+] Interpolated Quantety Successfully Found"
-        plt.plot(r_test, q_test, ".")
-        plt.show()
-        
+
+        if test:
+            plt.plot(r_test, q_test, ".")
+            plt.show()
+
         return values, flatten_values
 
 
     def bound_coord(self, coord, tp):
-        
+
         if coord == np.inf or coord != coord:
             if tp == "x":
                 coord = self.xlim[0] - 5
@@ -147,26 +194,26 @@ class ETInterpolater:
                 coord = self.ylim[1] + 5
             elif tp == "z":
                 coord = self.zlim[1] + 5
-        
-        return self.desymmetrize_coord(coord)
+
+        return coord#self.desymmetrize_coord(coord)
 
     def desymmetrize_coord(self, coord):
         return abs(coord)
 
 
-    
+
     def flatten_dict(self, d):
         flatten_values = []
         for index,domain in enumerate(d):
             for k in domain.keys():
                 for j in domain[k].keys():
-                    for r in range(len(domain[k][j])):  
+                    for r in range(len(domain[k][j])):
                         flatten_values.append(d[index][k][j][r])
 
         return flatten_values
 
 
-    
+
     def write_values_to_file(self, values, file):
         with open(file, "w") as f:
             for index,domain in enumerate(values):
@@ -193,27 +240,28 @@ class ETInterpolater:
         #np.savetxt(file, np.array(values))
 
 
-    def LORENE_read(self, origin=[0,0,0]):
-        p = subprocess.Popen("./get_points %s %s %s 1" %(origin[0], origin[1], origin[2]), stdout=subprocess.PIPE, shell=True)
+    def LORENE_read(self, filename, origin=[0,0,0]):
+        p = subprocess.Popen("../C/get_points %s %s %s 1 %s" %(origin[0], origin[1], origin[2], filename), stdout=subprocess.PIPE, shell=True)
+        #p = subprocess.Popen("./get_points %s %s %s 1" %(origin[0], origin[1], origin[2]), stdout=subprocess.PIPE, shell=True)
         (output, err) = p.communicate()
         p_status = p.wait()
         if p_status != 0:
             raise IOError("Could not read LORENE C code!")
         else:
             print "[+] LORENE Successfully Read and Saved the File"
-        
+
 
 
     def get_coll_points(self, origin=[0,0,0]):
-        p = subprocess.Popen("./get_points %s %s %s 0" %(origin[0], origin[1], origin[2]), stdout=subprocess.PIPE, shell=True)
+        p = subprocess.Popen("../C/get_points %s %s %s 0 none.txt" %(origin[0], origin[1], origin[2]), stdout=subprocess.PIPE, shell=True)
         (output, err) = p.communicate()
         p_status = p.wait()
         if p_status != 0:
             raise IOError("Could not read LORENE C code!")
         else:
             print "[+] LORENE Successfully Gave the Coll Points"
-        
-        print output
+
+        #print output
         x_dicts, y_dicts, z_dicts = self.clean_coll_points_xyz(output)
         return x_dicts, y_dicts, z_dicts
 
@@ -228,26 +276,26 @@ class ETInterpolater:
         """
         corrds = []
         index = 0
-        for index, sub_s in enumerate(s):        
+        for index, sub_s in enumerate(s):
                 if sub_s == "+":
                     break
         s = s[index+1:]
-        for index, sub_s in enumerate(s):        
+        for index, sub_s in enumerate(s):
                 if sub_s == "+":
                     break
-        
-        
+
+
         x_dicts = self.clean_coll_points(s[:index])
         s = s[index+1:]
-        
-        for index, sub_s in enumerate(s):        
+
+        for index, sub_s in enumerate(s):
                 if sub_s == "+":
                     break
 
         y_dicts = self.clean_coll_points(s[:index])
         s = s[index+1:]
 
-        for index, sub_s in enumerate(s):        
+        for index, sub_s in enumerate(s):
                 if sub_s == "+":
                     break
 
@@ -266,9 +314,9 @@ class ETInterpolater:
         s = s.split("\n")
         while len(s) > 0:
             for index, sub_s in enumerate(s):
-                
+
                 if "*** Tbl 3D" in sub_s:
-                    
+
                     break
             domains.append(s[:index-1])
             s = s[index+1:]
@@ -277,7 +325,7 @@ class ETInterpolater:
         domain_dicts = []
         for domain in domains:
             domain_dicts.append(self.get_domain_dict_from_string(domain))
-        
+
         return domain_dicts
 
     def get_domain_dict_from_string(self,domain_string):
@@ -287,41 +335,116 @@ class ETInterpolater:
         domain_dict = {}
         current_k = None
         for sub_s in domain_string:
-            
+
             if "k =" in sub_s:
                 current_k = int(sub_s.split()[2])
                 domain_dict[current_k] = {}
             elif "j = " in sub_s:
                 current_j = int(sub_s.split()[2])
-                
+
                 domain_dict[current_k][current_j] = [float(j) for j in sub_s.split()[4:]]
 
         return domain_dict
 
 
+    def super_gaussian(self, x, y, z, bh_pos, rad, I=1, n=10):
+        r = np.sqrt((bh_pos[0]-x)**2 + (bh_pos[1]-y)**2 + (bh_pos[2]-z)**2)/float(rad)
+        return 1-I*np.exp(-2*r**n)
+
+
+
+    def read_bbh_diag(self):
+        itbh1, tbh1, xbh1, ybh1, zbh1, rbh1 = np.loadtxt("%s/output-0000/bbh3d/BH_diagnostics.ah1.gp" %self.dir,
+                                                         usecols=(0,1,2,3,4,7),
+                                                         unpack=True)
+
+        itbh2, tbh2, xbh2, ybh2, zbh2, rbh2 = np.loadtxt("%s/output-0000/bbh3d/BH_diagnostics.ah2.gp" %self.dir,
+                                                         usecols=(0,1,2,3,4,7),
+                                                         unpack=True)
+
+
+        return np.array(itbh1), np.array([[xbh1, ybh1, zbh1], [xbh2, ybh2, zbh2]]), np.array([rbh1, rbh2])
+
+
+
+
+
+    def analyse_bbh(self, geometry, quantity, iterations, test=False):
+        possible_iterations, positions, radii = self.read_bbh_diag()
+
+        for it in iterations:
+            it_index = np.where(possible_iterations == it)
+
+            if len(it_index) == 0:
+                continue
+
+            pos1 = (positions[0,:,it_index])[0,0,:]
+            pos2 = (positions[1,:,it_index])[0,0,:]
+            radius1 = (radii[0,it_index])[0,0]
+            radius2 = (radii[1,it_index])[0,0]
+
+            if test:
+                self.make_test_bbh_plot(quantity, pos1, pos2, radius1, radius2)
+
+            q = self.read_ET_quantity(quantity, g, it, dimentions=3, order=4)
+
+            #BH1
+            print "[~] Starting with Black Hole 1"
+
+            values, flatten_values = self.get_values_at_coll_points(q,smooth=True, bh_pos=pos1, bh_rad=radius1,bh_pos2=pos2, bh_rad2=radius2)
+            filename = "../Python/%s_%s_body1.txt" %(quantity, it)
+            self.write_flatten_values_to_file(flatten_values, it, 1, filename)
+            self.LORENE_read(filename)
+
+
+            #BH2
+            print "[~] Now Black Hole 2"
+
+            values, flatten_values = self.get_values_at_coll_points(q,smooth=True, bh_pos=pos2, bh_rad=radius2,bh_pos2=pos1, bh_rad2=radius1)
+            filename = "../Python/%s_%s_body2.txt" %(quantity, it)
+            self.write_flatten_values_to_file(flatten_values, it, 2, filename)
+            self.LORENE_read(filename)
+
+
+            print "[+] Done!"
+
+
+
+
+
+
 
 if __name__=="__main__":
     #folder = "/media/dulte/Seagate Expansion Drive/Storage/Master/ET_data/GW150914_28"
-    folder = "/media/dulte/Seagate Expansion Drive/Storage/Master/ET_data/tov_ET_11"
+    #folder = "/media/dulte/Seagate Expansion Drive/Storage/Master/ET_data/tov_ET_11"
+    folder = "/mn/stornext/d13/euclid/daniehei/simulations/bbh_3D"
+    #folder = "/mn/stornext/d13/euclid/daniehei/simulations/tov_3D"
+
+    quantity = "kxx"
+    filename = "%s.txt" %quantity
     inter = ETInterpolater(folder)
     g = inter.make_geometry([-40, -40, -40], 200)
     it = 0
-    q = inter.read_ET_quantity("alp", g, it, dimentions=3, order=4)
-    
-    inter.make_test_plot("alp")
+
+    inter.analyse_bbh(g, quantity, [it], test=True)
+
+    #q = inter.read_ET_quantity(quantity, g, it, dimentions=3, order=4)
+
+    #inter.make_test_plot("gxx")
+    exit()
     values, flatten_values = inter.get_values_at_coll_points(q, test=False)
-    inter.write_flatten_values_to_file(flatten_values, it, 1, "flatten.txt")
-    inter.LORENE_read()
+    inter.write_flatten_values_to_file(flatten_values, it, 1, filename)
+    inter.LORENE_read(filename)
 
 
-    
+
     nz = 3
     nr = 8
     nt = 5
     np = 7
 
 
-    
+
     d = 0
     k = 1
     j = 3
